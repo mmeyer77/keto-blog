d5ebe94343ed4597c088f7fc20dee36646e5842d
Michael Meyer <meyermichael507@gmail.com>
2020-05-26 15:13:59 -0500

    build_creado

---
+++ client/build/precache-manifest.314d583379a5cd3f01c6c1f2714de058.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7a65b7583693dc1bb0561b84189d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06081e4015d0c0a0a986",
+    "url": "/static/css/main.371334c6.chunk.css"
+  },
+  {
+    "revision": "c3d9b603380f1b82110a",
+    "url": "/static/js/2.9d019d08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d019d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06081e4015d0c0a0a986",
+    "url": "/static/js/main.d809f324.chunk.js"
+  },
+  {
+    "revision": "ea8d77bfcfb06c3e4d27",
+    "url": "/static/js/runtime-main.1760d5bf.js"
+  },
+  {
+    "revision": "6706057a50e283e2d4f727d8ca898c5b",
+    "url": "/static/media/avocado_cheese3.6706057a.jpg"
+  },
+  {
+    "revision": "3082f6f217a95773823adec705aae078",
+    "url": "/static/media/dalgona_coffee.3082f6f2.jpg"
+  },
+  {
+    "revision": "cdeb83f7b245e8410a7961653cf58748",
+    "url": "/static/media/double-bubble.cdeb83f7.png"
+  },
+  {
+    "revision": "383a89dba1e3777d6493685777e17749",
+    "url": "/static/media/keto_composition.383a89db.jpg"
+  },
+  {
+    "revision": "b6d72b20be49b4fb04331c576055414c",
+    "url": "/static/media/keto_real.b6d72b20.png"
+  },
+  {
+    "revision": "f9aceecf6ae538db67f9317a360e4f35",
+    "url": "/static/media/leche_almendras.f9aceecf.jpeg"
+  },
+  {
+    "revision": "737e547e8920d5096c2491eb14c9cdbb",
+    "url": "/static/media/logo_keto.737e547e.png"
+  },
+  {
+    "revision": "d7d7a8653096eb30944fe3d25df428e0",
+    "url": "/static/media/logo_small.d7d7a865.jpg"
+  },
+  {
+    "revision": "33fec84bb4f2d2cfeb50ee34a70d257f",
+    "url": "/static/media/pancake_keto.33fec84b.jpg"
+  },
+  {
+    "revision": "4c69fd7bfa4e3d26bc1580b077032409",
+    "url": "/static/media/pancake_keto_396x703.4c69fd7b.jpg"
+  },
+  {
+    "revision": "33fec84bb4f2d2cfeb50ee34a70d257f",
+    "url": "/static/media/pancake_keto_750x750.33fec84b.jpg"
+  }
+]);
